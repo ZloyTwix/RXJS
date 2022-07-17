4bb29c1e39ce41b0611028f11c813e83aedc13fc
ZloyTwix <nikitos18.08@mail.ru>
2022-07-17 22:10:06 +0300

    change app.html and app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {debounceTime, Observable} from "rxjs";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+
+  change(text :string){
+    const seatch$ = new Observable(observer =>{
+      observer.next(text);
+    });
+     seatch$.pipe(debounceTime(500)).subscribe(value => {
+      console.log(value);
+    });
+  }
 }
